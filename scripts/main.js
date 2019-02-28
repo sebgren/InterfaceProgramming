d94fbe59aa394edb4284e1e309e1340a06e065f8
Sebastian Holmgren <seho3066@gmail.com>
2019-02-28 22:23:12 +0100

    Deleted some unused code in order and made the clickable table stop pulsating when order is marked as completed or aborted. also added buttons for completing or aborting order in tables

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,41 @@ function showCredit(userName) {
 	stringCredit.textContent = "Hi "+ userName + "! Your current credit is: " + credit;
 }
 
+
+/**
+FUNCTIONS FOR TABLES-VIEW
+**/
+
+//Displays the tab. currently, it will only display the hard-coded
+//tab for table one.
 function displayTab() {
-	document.getElementById('tab-content').style.display = "block";
-	console.log("asdf")
+	document.getElementById('tab-content').style.visibility = "visible";
+	showFooter(true);
 }
+
+//Adds the animation class and then removes it when it's done playing
+async function clickAnimation(btn){
+	//document.getElementById(btn).className = "btn-animation btn " + btn;
+	//await sleep(500);
+	//document.getElementById(btn).className = "btn " + btn;
+	if (document.getElementById('tab-footer').style.opacity == "1") {
+		document.getElementById('tab-content').style.visibility = "hidden";
+		showFooter(false);
+		document.getElementsByClassName("active-table")[0].style.animation = "none";
+	}
+
+}
+
+function showFooter(val) {
+	if (val) {
+		document.getElementById('tab-footer').style.opacity = "1";
+	} else {
+		document.getElementById('tab-footer').style.opacity = "0.4";
+	}
+}
+
+//below is a function to make the button click animation
+//work properly more than once
+//function sleep(ms) {
+  //return new Promise(resolve => setTimeout(resolve, ms));
+//}
