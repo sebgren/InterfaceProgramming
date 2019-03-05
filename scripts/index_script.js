deea30ecd6ff1d40fbdc694903cc897d59a970bb
Nibogdan <n.b_ro@yahoo.com>
2019-03-05 11:54:32 +0100

    Made a button to change the language

---
+++ scripts/index_script.js
@@ -0,0 +1,25 @@
+var text1 = '{"tab" : { "eng" : "Tab", "swe" : "Nota" }, "username" : { "eng" : "Username",   "swe" : "Användarnamn" },"password" : {"eng" : "Password", "swe" : "Lösenord" } }';
+var text2 ='{ "submit" : { "eng" : "Submit", "swe" : "Skicka" }, "regularguest" : { "eng" : "Regular Guest", "swe" : "Vanlig Gäst" } }';
+
+
+  var myObj=JSON.parse(text1);
+  var myObj2=JSON.parse(text2);
+
+
+lang1 = localStorage.getItem("lang");
+// alert(" "+myObj2.submit.swe);
+
+if (lang1=="swe") {
+document.getElementById("user").innerHTML=myObj.username.swe;
+document.getElementById("pass").innerHTML=myObj.password.swe;
+ document.getElementById("subm").innerHTML=myObj2.submit.swe;
+ document.getElementById("regguest").innerHTML=myObj2.regularguest.swe ; 
+   
+} 
+if (lang1=="eng") {
+document.getElementById("user").innerHTML=myObj.username.eng;
+document.getElementById("pass").innerHTML=myObj.password.eng;
+ document.getElementById("regguest").innerHTML=myObj2.regularguest.eng;
+
+}
+
