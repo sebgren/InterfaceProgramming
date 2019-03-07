bd91200f0112e9df5c5307a84c00f5cb5a80950b
Sebastian Holmgren <seho3066@gmail.com>
2019-03-07 10:39:41 +0100

    fixed crash on lang change, tablet is now called table and button removed in index

--- scripts/ch_lang.js
+++ scripts/ch_lang.js
@@ -10,13 +10,13 @@ lang1=localStorage.getItem("lang")
  localStorage.setItem("lang","swe");  
  lang1=localStorage.getItem("lang");
  document.getElementById("chlang").value="Svenska"
- setTimeout("location.href='/pr4-github/InterfaceProgramming/index.html'",2000)
+ setTimeout("location.href='index.html'")
  }
  else {
  localStorage.setItem("lang","eng");
  lang1=localStorage.getItem("lang");
  document.getElementById("chlang").value="English"
- setTimeout("location.href='/pr4-github/InterfaceProgramming/index.html'",2000)
+ setTimeout("location.href='index.html'")
  }
 
 
