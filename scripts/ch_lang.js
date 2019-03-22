de512f8a7bdad1ba009219d6d4ff27af550b2fe3
Sebastian Holmgren <seho3066@gmail.com>
2019-03-22 13:41:24 +0100

    bogdans comments

--- scripts/ch_lang.js
+++ scripts/ch_lang.js
@@ -1,8 +1,17 @@
+//language switcher 
+// Nicolae Bogdan 2019
 
+// set the language default to swedish and store on local storage
 
 lang1=localStorage.getItem("lang")
  if (lang1=="")
  localStorage.setItem("lang","swe")
+
+// function that changes language once the language button is clicked
+// if the language is english switch to swedish and vice versa
+// also store the new languate on local storage so that 
+// the translation.js script can set the language of all pages
+// the function reloads the main page so the changes are reflected on site.
 
  function change_lang() {
  lang1=localStorage.getItem("lang");
